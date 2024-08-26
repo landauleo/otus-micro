19bd60dd780101490d3e3d9b61e9e223ea9ad6da
landauleo <bambie7@mail.ru>
2024-08-27 01:46:55 +0300

    hw5

--- src/main/java/leo/landau/UserController.java
+++ src/main/java/leo/landau/UserController.java
@@ -2,6 +2,7 @@ package leo.landau;
 
 import javax.validation.constraints.NotNull;
 
+import io.micrometer.core.annotation.Timed;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,21 +15,25 @@ public class UserController {
     @Inject
     private UserServiceImpl userServiceImpl;
 
+    @Timed(value = "http.server.requests", histogram = true)
     @Post
     public HttpResponse<UserDto> createUser(@Body @NotNull UserDto user) {
         return HttpResponse.ok(userServiceImpl.createUser(user));
     }
 
+    @Timed(value = "http.server.requests", histogram = true)
     @Get("/{userId}")
     public HttpResponse<UserDto> findUserById(@PathVariable Long userId) {
         return HttpResponse.ok(userServiceImpl.findUserById(userId));
     }
 
+    @Timed(value = "http.server.requests", histogram = true)
     @Put("/{userId}")
     public HttpResponse<UserDto> updateUser(@PathVariable Long userId, @Body @NotNull UserDto user) {
         return HttpResponse.ok(userServiceImpl.updateUser(userId, user));
     }
 
+    @Timed(value = "http.server.requests", histogram = true)
     @Delete("/{userId}")
     public HttpResponse<Void> deleteUser(@PathVariable Long userId) {
         userServiceImpl.deleteUser(userId);
